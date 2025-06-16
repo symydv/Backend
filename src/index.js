585d06a79bda849c80bc1cedcd1f27b3d361926e
symydv <623175@student.nitandhra.ac.in>
2025-06-16 21:29:27 +0530

    setup project files part 1

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// we have used nodemon to constantly refresh the server whenever we edit our index.js,
+// for that we have installedd nodemon using "npm i -D nodemon" in our terminal inside package.json,
+// we also added "dev": "nodemon src/index.js" inside script in package.json
