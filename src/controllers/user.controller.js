deaa76ce40f96410ade48556043879c8df9dac03
symydv <623175@student.nitandhra.ac.in>
2025-07-04 22:51:21 +0530

    some update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -197,7 +197,7 @@ const logoutUser = asyncHandler(async(req, res) => {
         req.user._id,
         {
             $unset:{
-                refreshToken: 1
+                refreshToken: 1 //this removes the field from document
             }
         },
         {
