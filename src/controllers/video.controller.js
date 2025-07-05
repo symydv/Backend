91831b165534ef47ebfd56d41fee035fb6e09bef
symydv <623175@student.nitandhra.ac.in>
2025-07-05 13:24:12 +0530

    added getAllVideos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -10,6 +10,44 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    const pageNumber = parseInt(page)
+    const limitNumber = parseInt(limit)
+    const sortOrder = sortType === "desc" ? -1:1 ;
+
+    filter = {}
+    // 👇 If a userId is provided in query, filter by it
+    if (userId) {
+    filter.owner = userId;
+    }
+
+    // 👇 If a search query is provided, add case-insensitive title match
+    if (query) {
+    filter.title = { $regex: query, $options: "i" }; // 'i' = case-insensitive
+    }
+    // filter.title	You're building a MongoDB query object to match documents where the title field...
+    // { $regex: query }	...matches a regular expression pattern (basically a flexible search) based on the query string sent by the user
+    // $options: "i"	"i" means case-insensitive (so "Cat" matches "cat", "CAT", etc.)
+
+    const videos = await Video.find(filter)
+    .sort({[sortBy]: sortOrder} )              //.sort({ createdAt: -1 }) // Sort by createdAt descending
+    .skip((pageNumber-1)*limitNumber)          //This skips the first N documents depending on which page you're on.
+    .limit(limitNumber)                        // If limitNumber = 10, you get only 10 documents per page
+
+
+    const total = await Video.countDocuments(filter); //this line counts the total number of documents in our databse that matches the filter.
+
+    // return res.status(200).json(new ApiResponse(200, videos, "videos fetched successfully"))
+    return res.status(200).json({
+    success: true,
+    data: videos,
+    pagination: {
+        total,
+        page: pageNumber,
+        limit: limitNumber,
+        totalPages: Math.ceil(total / limitNumber)
+    }
+    });
+
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
